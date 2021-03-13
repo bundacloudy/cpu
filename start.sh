6cecd0eaa3859f4b9d7f8bdf1fe4bb628ac5cfba
bundacloudy <80420323+bundacloudy@users.noreply.github.com>
2021-03-13 14:38:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qnh6ejv7er2d4g7xdpe43p6vn958fuhvl9lsavk.test1
 
 
 done
